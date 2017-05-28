9eb438f8361ea84e674e93324de9777980198d7e
ramalratnayake <ramal.ratnayake@gmail.com>
2017-05-28 14:26:58 +1000

    Data structures and stuff

---
+++ Project/VendingMachine/modules/macros.asm
@@ -0,0 +1,142 @@
+; The macro clears a word (2 bytes) in a memory
+; the parameter @0 is the memory address for that word
+.macro clear
+    ldi YL, low(@0)     ; load the memory address to Y
+    ldi YH, high(@0)
+    clr temp 
+    st Y+, temp         ; clear the two bytes at @0 in SRAM
+    st Y, temp
+.endmacro
+
+.macro checkInStart
+    in temp, SREG
+    push temp
+    mov temp, inStart
+    cpi temp, 1                 ; checking whether the start screen is open
+    brne end                    ; not in start screen, so keep going
+    pop temp
+    out SREG, temp
+    pop temp
+    clr inStart
+    set_reg inSelect
+    rjmp main            ; if it is, tell main to change to Select screen
+
+    end:
+    pop temp
+    out SREG, temp
+    pop temp
+.endmacro
+
+.macro do_lcd_command
+    ldi r16, @0
+    rcall lcd_command
+    rcall lcd_wait
+.endmacro
+
+/*
+    @0 - register containing desired index (not temp!)
+    @1 - address of array to read from
+    @2 - register to write value to
+*/
+.macro get_element
+    push XL
+    push XH
+    push @0
+    push temp
+    in temp, SREG
+    push temp   
+    
+    ldi XL, low(@1)
+    ldi XH, high(@1)
+
+    loop:
+    cpi @0, 0
+    breq getVal
+    subi @0, 1
+    adiw XL:XH, 1
+    jmp loop
+    
+    getVal:
+    ld @2, X
+    
+    pop temp
+    out SREG, temp
+    pop temp
+    pop @0
+    pop XH
+    pop XL
+.endmacro
+
+/*
+    @0 - register containing desired index (not temp!)
+    @1 - address of array to write to
+    @2 - register to write value from
+*/
+.macro set_element
+    push XL
+    push XH
+    push @0
+    push temp
+    in temp, SREG
+    push temp
+    
+    ldi XL, low(@1)
+    ldi XH, high(@1)
+
+    loop:
+    cpi @0, 0
+    breq setVal
+    subi @0, 1
+    adiw XL:XH, 1
+    jmp loop
+    
+    setVal:
+    st X, @2
+
+    pop temp
+    out SREG, temp
+    pop temp
+    pop @0
+    pop XH
+    pop XL
+.endmacro
+
+.macro set_reg
+    push temp
+    ser temp
+    mov @0, temp
+    pop temp
+.endmacro
+
+.macro clr_reg
+    push temp
+    clr temp
+    mov @0, temp
+    pop temp
+.endmacro
+
+.macro do_lcd_data
+    push r16
+    mov r16, @0
+    subi r16, -'0'
+    rcall lcd_data
+    rcall lcd_wait
+    pop r16
+.endmacro
+
+.macro do_lcd_data_i
+	push r16
+	ldi r16, @0
+	rcall lcd_data
+	rcall lcd_wait
+	pop r16
+.endmacro
+
+.macro lcd_set
+    sbi PORTA, @0           //set a bit (specified by @0) in portA
+.endmacro
+
+.macro lcd_clr
+    cbi PORTA, @0           //clear a bit (specified by @0) in portA
+.endmacro
+
